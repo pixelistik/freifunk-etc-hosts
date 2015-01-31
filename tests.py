249310e42f871ceebb214054c00cc87ee0fe6a60
Pixelistik <code@pixelistik.de>
2015-01-31 17:10:53 +0100

    Prepare test data in each test case

--- tests.py
+++ tests.py
@@ -5,7 +5,12 @@ import os.path
 import json
 
 class TestConverter(unittest.TestCase):
-    simple_node = """
+
+    def setUp(self):
+        self.converter = freifunk_etc_hosts.Converter()
+
+    def test_parse_json(self):
+        simple_node = """
     {
         "c4:6e:1f:a2:97:ae": {
             "network": {
@@ -19,25 +24,33 @@ class TestConverter(unittest.TestCase):
         }
     }
 """
-    f = open('tests/fixtures/case-1/hosts', 'r')
-    case_1_expected = f.read()
-    f.close()
 
-    def setUp(self):
-        self.converter = freifunk_etc_hosts.Converter()
-
-    def test_parse_json(self):
         expected = "2a03:2260:40:0:c66e:1fff:fea2:97ae	testhost\n"
-        result = self.converter._parse_json(json.loads(self.simple_node))
+        result = self.converter._parse_json(json.loads(simple_node))
 
         self.assertEqual(result, expected)
 
     def test_convert(self):
+        f = open('tests/fixtures/case-1/hosts', 'r')
+        expected = f.read()
+        f.close()
+
         f = open('tests/fixtures/case-1/alfred_merged.json', 'r')
         result = self.converter.convert(f)
         f.close()
 
-        self.assertEqual(str(result), str(self.case_1_expected))
+        self.assertEqual(str(result), str(expected))
+
+    def test_alphabetical_order(self):
+        f = open('tests/fixtures/alphabetical_order/hosts', 'r')
+        expected = f.read()
+        f.close()
+
+        f = open('tests/fixtures/alphabetical_order/alfred_merged.json', 'r')
+        result = self.converter.convert(f)
+        f.close()
+
+        self.assertEqual(str(result), str(expected))
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
